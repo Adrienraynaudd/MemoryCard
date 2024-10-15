ed8a0faa3cfeb88ed4f36919d2ce2797c7a6d79e
adrienraynaudd <adrien48632@gmail.com>
2024-10-15 13:51:18 +0200

    link user

--- MemoryCard/src/app/Service/user.service.ts
+++ MemoryCard/src/app/Service/user.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { register } from 'module';
+import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl: any = {
+  private apiUrl = {
     register: "http://localhost:3000/api/users/",
     login: "http://localhost:3000/api/users/login",
     updateUser: "http://localhost:3000/api/users/:id",
-    }
-    constructor(private http: HttpClient) { }
+  }
 
-    register(user: User): Observable<any> {
-      return this.http.post(this.apiUrl.register, user);
+  constructor(private http: HttpClient) { }
+
+  register(user: User): Observable<any> {
+    return this.http.post(this.apiUrl.register, user);
+  }
+
+  login(user: User): Observable<any> {
+    return this.http.post(this.apiUrl.login, user);
+  }
+
+  updateUser(user: User): Observable<any> {
+    if (user.id) {
+      return this.http.put(this.apiUrl.updateUser.replace(':id', user.id.toString()), user);
+    } else {
+      throw new Error("L'ID de l'utilisateur est manquant.");
     }
-    login(user: User): Observable<any> {
-      return this.http.post(this.apiUrl.login, user);
-    }
-    updateUser(user: User): Observable<any> {
-      return this.http.put(this.apiUrl.updateUser, user);
-    }
+  }
 }
