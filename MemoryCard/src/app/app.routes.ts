a5891089371b9064d506f19ffe8ea43e94468f5f
Tommy <tommy.angibaud@gmail.com>
2024-10-17 20:20:31 +0200

    Move component in a new page

--- MemoryCard/src/app/app.routes.ts
+++ MemoryCard/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AddCardComponent } from './add-card/add-card.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'addCardComponent', component: AddCardComponent },
     { path: '**', redirectTo: '' },
 ];
